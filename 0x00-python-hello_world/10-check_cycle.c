00f722bf857a21fe989125ec5c468b5d7273a229
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-10-02 18:30:46 -0700

    testing-code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,28 +1,25 @@
 #include "lists.h"
 
 /**
- * check_cycle - function checks if cycle exists
- * @list: the header of list
- * Return: 0 if no cycle or 1 if found
+ * check_cycle - check for loop in LL
+ * @list: head of linked list
+ * Description - check for loops in LL
+ * Return: 1 if cycled, 0 if not
  */
+
 int check_cycle(listint_t *list)
 {
-
-	listint_t *slow;
-	listint_t *fast;
-
+	listint_t *slow = list;
+	listint_t *fast = list->next;
+	
 	if (list == NULL || list->next == NULL)
-		return (0);
-	slow = list;
-	fast = lis->next;
-
-	while (slow && fast && fast->next)
+	return (0);
+	while (slow != fast)
 	{
-		if (slow == fast)
-			return (1);
-
+		if (fast == NULL || fast->next == NULL)
+			return 0;
 		slow = slow->next;
 		fast = fast->next->next;
 	}
-	return (0);
+	return (1);
 }
