93e86beca40bca1430f0275f2aa20134b8996f20
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-10-03 17:11:27 -0700

    adding-code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 for i in range(0, 10):
-    for b in range(i + 1, 9):
-        print("{}".format(i),end="")
-        print("{}".format(b),end=", ")
-print("89")
+    for b in range(i + 1, 10):
+        if i == 8 and b == 9:
+            print("{}{}".format(i, b))
+        else:
+            print("{}".format(i),end="")
+            print("{}".format(b),end=", ")
