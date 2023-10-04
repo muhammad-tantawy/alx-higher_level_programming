e4d9ee8fae444cf8d39d59549393fe1a2d59b413
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-10-03 18:33:10 -0700

    adding-code

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 
 #def fizzbuzz():
 for i in range(1, 101):
-    if i % 3 == 0:
-        i = 'Fizz'
+    if i % 15 == 0:
+        i = 'FizzBuzz'
     elif i % 5 == 0:
         i = 'Buzz'
-    elif i % 15 == 0:
-        i = 'FizzBuzz'
+    elif i % 3 == 0:
+        i = 'Fizz'
     print("{}".format(i), end=" ")
