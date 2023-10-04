8054e58e6a1c57ab708ac2b3786fd4824bb0b7e3
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-10-03 17:59:10 -0700

    adding-code

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,4 +4,4 @@ def print_last_digit(number):
     if number > 0:
         print("{}".format(last),end="")
     elif number < 0:
-        print("{}".format(last * -1),end="")
+        print("{}{}".format(last, last),end="")
