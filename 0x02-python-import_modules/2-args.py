87f36658587ff8dd3ce18d6725463cf748a4cc2d
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-10-05 13:57:00 -0700

    adding-code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
         print("1 argument:")
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
