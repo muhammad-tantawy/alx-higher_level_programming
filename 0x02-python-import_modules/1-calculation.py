ba96f779d32113afb4daafe653c97916902c9ed5
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-10-05 11:42:49 -0700

    adding-code

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import calculator_1
-    a = 10
-    b = 5
-    print ("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-    print ("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-    print ("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-    print ("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+    a = c =  10
+    b = d = 5
+    print ("{} + {} = {}".format(c, d, calculator_1.add(a, b)))
+    print ("{} - {} = {}".format(c, d, calculator_1.sub(a, b)))
+    print ("{} * {} = {}".format(c, d, calculator_1.mul(a, b)))
+    print ("{} / {} = {}".format(c, d, calculator_1.div(a, b)))
