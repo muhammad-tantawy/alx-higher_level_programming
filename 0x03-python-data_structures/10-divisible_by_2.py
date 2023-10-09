bf7b14dc6602acc6dda4374580a39cc0793a9f8b
muhammad-tantawy <muhammad.tantawy@outlook.com>
2023-10-09 17:48:02 +0300

    adding_code

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list
+    new_list = []
     for i in new_list:
         if i % 2 == 0:
-            i = True
+            new_list.append()= True
         else:
-            i = False
+            new_list.append() = False
