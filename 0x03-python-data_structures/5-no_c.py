588ec53a941c80d07771b00adc934aa8f991b355
muhammad-tantawy <muhammad.tantawy@outlook.com>
2023-10-09 16:03:25 +0300

    adding_code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        new_string = ''
-        for c in my_string:
-            if c.lower() != 'c':
-                new_string += c
-        return (new_string)
+    new_string = ''
+    for c in my_string:
+        if c.lower() != 'c':
+            new_string += c
+    return (new_string)
