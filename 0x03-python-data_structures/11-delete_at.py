66e350d9fa8a83a029d8e7f97feacc916e8ecb6b
muhammad-tantawy <muhammad.tantawy@outlook.com>
2023-10-09 23:56:43 +0300

    adding_code

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,5 +5,7 @@ def delete_at(my_list=[], idx=0):
         return (my_list)
     if idx > num:
         return(my_list)
-    my_list = my_list[0:idx] + my_list[idx + 1:]
-    return (my_list)
+    new_list = ()
+    new_list = my_list[0:idx] + my_list[idx + 1:]
+    my_list = new_list
+    return (new_list)
