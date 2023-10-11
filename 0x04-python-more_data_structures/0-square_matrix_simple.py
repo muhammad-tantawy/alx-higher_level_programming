608e10b15f90ac8db87e6ec00b01546417b9914a
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-10-11 11:35:34 -0700

    adding-code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-     """
-    A function that computes the square
-    value of all integers of a matrix.
-    """
+def square_matrix_simple(matrix):
     new_matrix = []
     for col in matrix:
-        for x in col:
-            result = x**2
-            new_matrix.append(result)
+        result = list(map(lambda x: x**2, col))
+        new_matrix.append(result)
     return new_matrix
