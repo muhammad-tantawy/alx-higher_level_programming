1f0a11c5d20e3efc204c0899dcb3658b66bc97d5
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-10-11 16:38:28 -0700

    adding-code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
+    new_dictionary = {}
     for key, value in a_dictionary.items():
-        new_dict.update({key: (value * 2)})
+        new_dictionary.update({key: (value * 2)})
     return new_dictionary
